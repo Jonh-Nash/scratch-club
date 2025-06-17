c8cfe12f5b772885d168fd33776df1e5d736eee7
justy <vicious8424687@gmail.com>
2025-06-17 22:30:14 +0900

    新しいファイルを追加: eda.ipynb、forward_net.py、nagative_sampling_layer.py、word2vec_fast.ipynb、layers.py、gt.py

---
+++ deep-learning-2nd/src/common/layers.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+    
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+    
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+        np.add.at(dW, self.idx, dout)
+        return None
+        
+class MatMul:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.x = None
+    
+    def forward(self, x):
+        W, = self.params
+        out = np.dot(x, W)
+        self.x = x
+        return out
+    
+    def backward(self, dout):
+        W, = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        self.grads[0][...] = dW
+        return dx
+    
