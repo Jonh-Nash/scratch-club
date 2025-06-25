f1421a4f3116f6aca9d4e67306cdb796f6a8d41b
justy <vicious8424687@gmail.com>
2025-06-26 01:05:02 +0900

    functions.pyにsoftmax関数を実装

--- deep-learning-2nd/src/common/functions.py
+++ deep-learning-2nd/src/common/functions.py
@@ -1,1 +1,11 @@
-from common.functions import softmax, cross_entropy_error
+import numpy as np
+
+def softmax(x):
+    if x.ndim == 2:
+        x = x - np.max(x, axis=1, keepdims=True)
+        x = np.exp(x)
+        x /= x.sum(axis=1, keepdims=True)
+    if x.ndim == 1:
+        x = x - np.max(x)
+        x = np.exp(x)
+        x /= np.sum(x)
