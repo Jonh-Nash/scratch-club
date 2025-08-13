1d2e7445a10df027905be3c6e7a1696337d26a36
justy <vicious8424687@gmail.com>
2025-08-13 22:53:29 +0900

    add selection sort

---
+++ dsa/selection-sort.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def sort(numbers: List[int]) -> List[int]:
+    for i in range(len(numbers)):
+        min_num_index = i
+        for j in range(i, len(numbers)):
+            if numbers[min_num_index] > numbers[j]:
+                min_num_index = j
+        if numbers[i] > numbers[min_num_index]:
+            numbers[i], numbers[min_num_index] = numbers[min_num_index], numbers[i]
+        
+    return numbers
+        
+if __name__ == '__main__':
+    import random
+    nums = [random.randint(0, 1000) for i in range(10)]
+    print(sort(nums))
